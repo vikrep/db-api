53ef71b485e63ad4186c97ebb35da630bc7b391d
vikrep <protsenko.viktor@gmail.com>
2018-08-09 17:17:16 +0100

    change URL

--- server.js
+++ server.js
@@ -2,24 +2,24 @@ const cool = require('cool-ascii-faces')
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const pg = require('pg');
+const {Pool} = require('pg');
 const PORT = 5000;
 // load all env variables from .env file into process.env object.
 require('dotenv').config()
 
-let pool = new pg.Pool({
-    port: 5432,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    max: 10
-});
+// let pool = new Pool({
+//     port: 5432,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     max: 10
+// });
 
-// let pool = new pg.Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: true
-// })
+let pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+})
 
 // Setup the express app
 
@@ -44,7 +44,7 @@ app.get('/api/albums', (req, res) => {
         if (err) {
             res.status(500).json({ error: err });
         } else {
-            db.query('SELECT cover, artist, title, year, rating, id FROM artist, title WHERE artist.ref_id = title.ref_id',
+            db.query('SELECT cover, artist, title, year, rating, id FROM artist, title WHERE artist.ref_id = title.ref_id;',
                 (err, table) => {
                     if (err) {
                         res.status(500).json({ error: err });
@@ -59,4 +59,6 @@ app.get('/api/albums', (req, res) => {
 
 app.get('/cool', (req, res) => res.send(cool()))
 
-app.listen(PORT, () => console.log('Listening on port ' + PORT));
+app.listen(process.env.PORT || PORT, function() {
+    console.log("Llistening on port" + PORT);
+  });
