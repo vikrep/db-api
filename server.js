8a4f2a3c458ebbeb544efa3d54e48b092bcd03b7
vikrep <protsenko.viktor@gmail.com>
2018-08-09 10:12:37 +0100

    change URL

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { Pool, Client } = require('pg');
 const PORT = 5000;
 const connectionString = 'postgres://kghpydsncehrre:f5f822411a913a9e8a73caeae8112752a75ad62dbc54e6d15c5fb06e4e667c3a@ec2-50-17-250-38.compute-1.amazonaws.com:5432/d2etfsda68egsh'
 // load all env variables from .env file into process.env object.
-require('dotenv').config()
+// require('dotenv').config()
 
 // let pool = new Pool({
 //     port: 5432,
@@ -45,15 +45,21 @@ app.use(function (req, res, next) {
 });
 
 
-client.connect()
-client.query('SELECT cover, artist, title, year, rating, id FROM artist, title WHERE artist.ref_id = title.ref_id;',
-    (err, res) => {
-        if (err) throw err;
-        for (let row of res.rows) {
-            console.log(JSON.stringify(row));
-        }
-        client.end();
-    })
+    client.connect()
+            client.query('SELECT cover, artist, title, year, rating, id FROM artist, title WHERE artist.ref_id = title.ref_id;',
+                (err, res) => {
+                    if (err) {
+                        res.status(500).json({ error: err });
+                    } else {
+                        // res.status(200).send(res.rows)
+                        console.log(res.rows)
+                    }
+                    client.end();
+                })
+                
+
+
+
 
 
 
